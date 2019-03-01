159d046047bf40e69f80238111b5306c5293c05d
Moritz Ulmer <Moritz.Ulmer@dlr.de>
2019-03-01 09:27:45 +0100

    Remove floor-planner component
    Why:
    
    - It is not used

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FloorPlannerComponent } from './floor-planner/floor-planner.component';
 import { EcsTestComponent } from './ecs-test/ecs-test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FloorPlannerComponent,
     EcsTestComponent
   ],
   imports: [
